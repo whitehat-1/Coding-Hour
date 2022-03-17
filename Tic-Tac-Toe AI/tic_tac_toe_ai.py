e3857dc78f6f6d3145fe878e07afd3c1509addac
Angie <sacconeangela12@gmail.com>
2022-03-16 22:08:53 -0400

    Update tic_tac_toe_ai.py

--- Tic-Tac-Toe AI/tic_tac_toe_ai.py
+++ Tic-Tac-Toe AI/tic_tac_toe_ai.py
@@ -1,1 +1,15 @@
+# Board setup for the Tic-Tac-Toe game by using a dictionary.
+# Tic_tac_toe_Board = {"":,}
 
+# Function to store and print out the board.
+def board_contents(Tic_tac_toe_Board):
+  # print board elements underneath here
+
+ #board keys
+ board_elements = []
+  
+# Tic-Tac-Toe Game function
+def ticTacToeGame():
+  # variables for X and O's in this game
+  
+  # if/elif/else statements to decide which moves the computer and user will make.
